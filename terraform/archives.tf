fd2d3d55bb7e9bb8a3d68bc0de50c07200a695dd
skuczynska <skuczynska@pgs-soft.com>
2021-11-02 10:33:55 +0100

    Add two data archive_files: modyf and to dynamo payload

--- terraform/archives.tf
+++ terraform/archives.tf
@@ -4,3 +4,15 @@ data "archive_file" "lambda-POST-presignedURL-zip" {
   source_file = "../src/lambda_POST_presignedURL.py"
   output_path = "lambda-POST-presignedURL.zip"
 }
+
+data "archive_file" "lambda-modyf-image-zip" {
+  type        = "zip"
+  source_file = "../src/lambda_modyf_image.py"
+  output_path = "lambda_modyf_image.zip"
+}
+
+data "archive_file" "lambda_to_dynamo_payload-zip" {
+  type        = "zip"
+  source_file = "../src/lambda_to_dynamo_payload.py"
+  output_path = "lambda_to_dynamo_payload.zip"
+}
