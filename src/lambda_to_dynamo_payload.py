a9d4b929251410b4eca63c8ae06b49effca6ced3
skuczynska <skuczynska@pgs-soft.com>
2021-11-02 10:55:45 +0100

    Add lambda dynamo db

---
+++ src/lambda_to_dynamo_payload.py
@@ -0,0 +1,24 @@
+import json
+import boto3
+from botocore.client import Config
+
+def lambda_handler(event, context):
+    """"Receive messages from sqs and send data to DynamoDB"""
+    client = boto3.client('dynamodb')
+    for message in event["Records"]:
+        msg = json.loads(message['body'])
+        item = {
+            "Image name": {
+                "S": msg["Image name"],
+            },
+            "Format": {
+                "S": msg["Format"],
+            },
+            "Height": {
+                "N": str(msg["Height"]),
+            },
+            "Width": {
+                "N": str(msg["Width"]),
+            },
+        }
+        response = client.put_item(TableName='picture', Item=item)
