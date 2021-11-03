84ebd5a2cfcd036cf5f4ff52b810d62458538dcc
skuczynska <skuczynska@pgs-soft.com>
2021-11-03 12:26:48 +0100

    Change queue from fifo to normal

--- terraform/queue.tf
+++ terraform/queue.tf
@@ -1,6 +1,13 @@
 # Queue
-resource "aws_sqs_queue" "skuczynska_queue" {
-  name                        = "skuczynska_queue.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
+resource "aws_sqs_queue" "queue" {
+  name                      = "skuczynska-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_to_dynamo" {
+  event_source_arn = aws_sqs_queue.queue.arn
+  function_name    = aws_lambda_function.sqs_to_dynamo.arn
 }
