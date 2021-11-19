91ecae46adee53fd681ed2ea522f5b5f72a38e96
skuczynska <skuczynska@pgs-soft.com>
2021-11-19 10:27:49 +0100

    Add modules/dynamodb/main.tf just to check terragrunt

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+
 resource "aws_dynamodb_table" "images-dynamodb-table" {
   name           = "skuczynska-images-dynamodb"
   hash_key       = "Image name"
